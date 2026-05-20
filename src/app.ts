dea63b6fbf0ad7e87806247c959b6aaabc3716cb
Ahmed Elselly <ahmedelselly87@gmail.com>
2026-05-20 19:05:59 +0300

    added initial routing and server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import routes from './routes';
+import { env } from './common/config/env';
 
+export const startServer = () => {
+	const app = express();
+
+	app.use(cors());
+	app.use(express.json());
+
+	app.use("/api", routes);
+
+	return app
+}
